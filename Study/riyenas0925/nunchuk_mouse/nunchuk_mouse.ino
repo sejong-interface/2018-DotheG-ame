2b706d6b30d214f9b03a543ecf57d0fd37f82e95
riyenas0925 <riyenas0925@gmail.com>
2018-07-16 18:51:32 +0900

    소스코드 정리

--- Study/riyenas0925/nunchuk_mouse/nunchuk_mouse.ino
+++ Study/riyenas0925/nunchuk_mouse/nunchuk_mouse.ino
@@ -6,60 +6,59 @@
 int nunchuk_data[6];
 
 int nunchuk_read(){
-    int i;
+  int i;
     
-    Wire.requestFrom(NUNCHUK_ADDRESS, 6);
+  Wire.requestFrom(NUNCHUK_ADDRESS, 6);
 
-    for (i = 0; i < 6 && Wire.available(); i++) {
-        nunchuk_data[i] = Wire.read();
-    }
-    
-    
-    Wire.beginTransmission(NUNCHUK_ADDRESS);
-    Wire.write(0x00);
-    
-    Wire.endTransmission();
-    return i == 6;
+  for (i = 0; i < 6 && Wire.available(); i++){
+    nunchuk_data[i] = Wire.read();
+  }
+      
+  Wire.beginTransmission(NUNCHUK_ADDRESS);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  
+  return i == 6;
 }
 
 void nunchuk_init(){
-   Wire.begin();
-   Wire.setClock(400000);
+  Wire.begin();
+  Wire.setClock(400000);
    
-   Wire.beginTransmission(NUNCHUK_ADDRESS);
-   Wire.write(0xF0);
-   Wire.write(0x55);
-   Wire.endTransmission();
+  Wire.beginTransmission(NUNCHUK_ADDRESS);
+  Wire.write(0xF0);
+  Wire.write(0x55);
+  Wire.endTransmission();
 }
 
 int nunchuk_buttonZ() {
-   return (~nunchuk_data[5] >> 0) & 1;
+  return (~nunchuk_data[5] >> 0) & 1;
 }
 
 int  nunchuk_buttonC() {
-   return (~nunchuk_data[5] >> 1) & 1;
+  return (~nunchuk_data[5] >> 1) & 1;
 }
 
-int nunchuk_joystickX_raw() {
-   return nunchuk_data[0];
+int nunchuk_joystickX() {
+  return nunchuk_data[0];
 }
 
-int nunchuk_joystickY_raw() {
-   return nunchuk_data[1];
+int nunchuk_joystickY() {
+  return nunchuk_data[1];
 }
 
 void setup() {
-   Serial.begin(9600);
-   Mouse.begin();
-   nunchuk_init();
+  Serial.begin(9600);
+  Mouse.begin();
+  nunchuk_init();
 }
 
-void loop() {   
+void loop() {
   if(nunchuk_read()){
     if (nunchuk_buttonZ() == HIGH) {
       Mouse.press(MOUSE_LEFT);
     }
-
+    
     else if(nunchuk_buttonC() == HIGH) {
       Mouse.press(MOUSE_RIGHT);
     }
@@ -69,5 +68,5 @@ void loop() {
       Mouse.release(MOUSE_RIGHT);
     }
   }
-   delay(10);
+  delay(10);
 }
