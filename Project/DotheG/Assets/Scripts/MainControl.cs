41e207d375b01ce26efb4de3971a2efd9f660bce
2kyung19 <32586712+2kyung19@users.noreply.github.com>
2018-09-02 02:10:19 +0900

    DotheG 총알 적용까지 완료

---
+++ Project/DotheG/Assets/Scripts/MainControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainControl : MonoBehaviour {
+
+    public Animator animator;
+    public Rigidbody rigidbody;
+
+    private float h;
+    private float v;
+
+    private float moveX;
+    private float moveZ;
+    private float speedH = 50f;
+    private float speedZ = 80;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.Play("JUMP00", -1, 0);
+        }
+
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        if (moveZ <= 0) {
+            moveX = 0;
+        }
+
+        rigidbody.velocity = new Vector3(moveX, 0, moveZ);
+	}
+}
---
+++ Project/DotheG/Assets/Scripts/Play_button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_button : MonoBehaviour {
+
+    void Awake()
+    {
+    }
+
+    public void StartButton() {
+        Invoke("startgame", .5f);
+    }
+
+    void startgame() {
+        Application.LoadLevel("Desert");
+    }
+}
