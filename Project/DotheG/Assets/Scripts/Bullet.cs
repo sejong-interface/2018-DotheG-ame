ae160a83bc371540ba8f3c2e3e9f423309a61662
kej <kej11070222@gmail.com>
2018-09-03 18:13:51 +0900

    ESC화면 기본 틀/UserUI 존재/총알 수정

---
+++ Project/DotheG/Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	void OnCollisionEnter(Collision Collision)
+	{
+		//충돌한 상대방에게 데미지를 주는 코드 작성
+
+		
+		Destroy(gameObject);
+	}
+}
