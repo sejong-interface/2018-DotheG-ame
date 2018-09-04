833311f4e789097cdd0c83fa3aaceb3e8e1128a6
kej <kej11070222@gmail.com>
2018-09-04 18:55:07 +0900

    ESC메뉴 스크립트 작성중/Start 씬 메인 카메라에 있는 Play_button스크립트 GameManager로 옮김->씬마다 GameManger 만들어서 관리하기

---
+++ Project/DotheG/Assets/Scripts/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	private GameObject ESCPanel;
+	private bool pause=false;
+	
+	void Awake() {
+		ESCPanel=GameObject.Find("Canvas").transform.Find("ESCPanel").gameObject;
+	}
+
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			pause=true;
+		}
+
+		if (pause==true){
+			Time.timeScale=0;
+			ESCPanel.SetActive(true);
+		}
+	}
+
+	public void StartBt(){
+		Debug.Log("게임 재시작");
+		Time.timeScale=1.0f;
+		SceneManager.LoadScene("Desert");
+	}
+	
+	public void MainBt(){
+		Debug.Log("메인으로");
+		Time.timeScale=1.0f;
+		SceneManager.LoadScene("Start");
+	}
+
+	public void ContinueBt(){
+		Debug.Log("게임 진행");
+		ESCPanel.SetActive(false);
+		Time.timeScale=1.0f;
+		pause=false;
+	}
+}
