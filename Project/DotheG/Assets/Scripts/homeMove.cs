89db3cec33f3549169370af0290c552f12884d69
2kyung19 <2kyung19@gmail.com>
2018-12-06 10:35:53 +0900

    /

---
+++ Project/DotheG/Assets/Scripts/homeMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class homeMove : MonoBehaviour {
+
+    void Awake()
+    {
+    }
+
+    public void StartButton()
+    {
+        Invoke("home", .5f);
+    }
+
+    void startgame()
+    {
+        Application.LoadLevel("Start");
+    }
+}
